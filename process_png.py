b48a9d9771ffd1f87cdb529baf45626cc2c4e42b
Igor Smirnov <smirnoviv@rambler.ru>
2019-05-19 10:57:28 +0500

    Add option -v to process_png.py

--- process_png.py
+++ process_png.py
@@ -4,12 +4,13 @@ from model import load_model
 from os import listdir
 from os.path import join, isfile
 from argparse import ArgumentParser
-from utils import image_to_tensor, get_device, probs_to_image, load_config
+from utils import image_to_tensor, get_device, probs_to_image, load_config, visualize
 from cv2 import imread, imwrite
 
 
 def main():
     parser = ArgumentParser(description='Segmenting .png files')
+    parser.add_argument('-v', action='store_true', required=False, default=False, help='Mix input and output')
     parser.add_argument('input_dir', type=str, help='Input directory')
     parser.add_argument('output_dir', type=str, help='Output directory')
     args = parser.parse_args()
@@ -21,9 +22,12 @@ def main():
         path = join(args.input_dir, fn)
         if fn.endswith('.png') and isfile(path):
             print('Processing %s' % path)
-            data = image_to_tensor(imread(path), device=device)
+            source = imread(path)
+            data = image_to_tensor(source, device=device)
             data = model(data)[0]
             result = probs_to_image(data)
+            if args.v:
+                result = visualize(source, result)
             imwrite(join(args.output_dir, fn), result)
     print('Completed')
 
